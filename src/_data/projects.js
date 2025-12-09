4506ca2006b0d6b0c1821ecd5a13481fcf64491c
Dominic Critchlow <critchlowd49@gmail.com>
2025-12-09 07:13:35 +0100

    typo fix

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    title: "Airwave ML",
+    title: "airwave ML",
     shortname: "airwave-ml",
     url: "https://github.com/DomCritchlow/airwave-ml",
     github: "https://github.com/DomCritchlow/airwave-ml",
